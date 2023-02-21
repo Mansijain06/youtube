e6ae55f9137386a86dd585be9e5dd8f1418850c5
Mansi Jain <70169492+Mansijain06@users.noreply.github.com>
2023-02-21 23:45:05 +0530

    formatted

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const ChatMessage = ({ name, message }) => {
     return (
-        <div className='flex items-center mb-4'>
+        <div className="flex items-center mb-4">
             <img
                 className="h-9"
                 src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
                 alt="user-icon"
             />
-            <span className='font-bold px-2'>{name} <span className='font-normal'>{message}</span></span>
+            <span className="font-bold px-2">
+                {name} <span className="font-normal">{message}</span>
+            </span>
         </div>
-    )
-}
+    );
+};
 
-export default ChatMessage
+export default ChatMessage;
